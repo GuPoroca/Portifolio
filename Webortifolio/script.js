ade6a89c9c009d557428785a4ba80a55fb6d2b9e
GPoroca <109002143+GuPoroca@users.noreply.github.com>
2022-07-17 20:43:43 -0300

    Git/curriculo
    yep

--- Webortifolio/script.js
+++ Webortifolio/script.js
@@ -1,17 +1,27 @@
 const list = document.querySelectorAll('.list');
 const parag = document.querySelectorAll('.parag');
 var atual = 0;
-var passado = 5;
+var passado = 0;
+var nome = "abt";
 
 
 function activeLink(){
+    atual = this.id;
+    if(atual==2){
+        window.open("https://github.com/GuPoroca", "_blank");
+        atual=0;
+        return;
+    }
     list.forEach((item) =>
     item.classList.remove('active'));
     this.classList.add('active');
-    document.getElementById(atual).style.opacity = 0;
-    atual = this.id;
-    document.getElementById(atual).style.opacity = 1;
+    document.getElementById(passado).style.opacity = 0;
+    document.getElementById(passado).className = nome;
     passado = atual;
+    document.getElementById(atual).style.opacity = 1
+    nome = document.getElementById(atual).className;
+    document.getElementById(atual).className += " active";
+    
 }
 
 function foo(){
