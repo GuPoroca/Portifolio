4ac2f387973834cf7dd743431f5f2e862b7477b3
GPoroca <93682321+GPoroca@users.noreply.github.com>
2022-07-17 18:08:44 -0300

    mainframe
    Main frame for texts working,
    -todo:
    (github link)
    (Lang change)
    (write the texts)

--- Webortifolio/script.js
+++ Webortifolio/script.js
@@ -1,8 +1,23 @@
 const list = document.querySelectorAll('.list');
+const parag = document.querySelectorAll('.parag');
+var atual = 0;
+var passado = 5;
+
+
 function activeLink(){
     list.forEach((item) =>
     item.classList.remove('active'));
     this.classList.add('active');
+    document.getElementById(atual).style.opacity = 0;
+    atual = this.id;
+    document.getElementById(atual).style.opacity = 1;
+    passado = atual;
 }
+
+function foo(){
+    document.getElementById(0).style.opacity = 1;
+}
+
 list.forEach((item) =>
 item.addEventListener('click',activeLink));
+
